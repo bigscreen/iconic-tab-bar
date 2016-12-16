8938af7cda1f6eb88ec441c3659586bd0a7e2496
Gallant Pratama <gallantpratama@go-jek.com>
2016-12-16 15:11:58 +0700

    Add vector drawable capability on demo

--- app/src/main/java/com/bigscreen/iconictabbardemo/MainActivity.java
+++ app/src/main/java/com/bigscreen/iconictabbardemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.bigscreen.iconictabbardemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.widget.TextView;
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
 
         initToolbar();
         initViews();
